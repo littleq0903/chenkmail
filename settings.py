9dfd9e2374498d69c236b531a9a429f5f70742de
LittleQ <littleq0903@gmail.com>
2011-10-02 10:40:04 +0800

    added static_root

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # Django settings for chenkmail project.
 import os
 
-PROJECT_ROOT = os.path.dirname(__file__)
+SITE_ROOT = os.path.dirname(__file__)
 
 
 DEBUG = True
@@ -17,7 +17,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'chenkmail.sqlite3',                      # Or path to database file if using sqlite3.
+        'NAME': 'chenkmail.sqlite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -61,7 +61,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
